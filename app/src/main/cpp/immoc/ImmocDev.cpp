9d308c140bad152a3da79ab3481100b8d16de1ea
qqhahaboy <122094709@qq.com>
2020-10-31 17:31:23 +0800

    剪切视频

--- app/src/main/cpp/immoc/ImmocDev.cpp
+++ app/src/main/cpp/immoc/ImmocDev.cpp
@@ -81,9 +81,10 @@ Java_com_example_mediadev_mooc_MoocActivity_mp4ToFlv(JNIEnv *env, jobject thiz,
             return;
         }
     }
+    //写入头部信息
     ret = avformat_write_header(output_fmt_ctx, NULL);
     if (ret < 0) {
-        LOGD("输出流头部信息写入失败");
+        LOGD("输出文件头部信息写入失败");
         return;
     }
     while (1) {
@@ -119,5 +120,136 @@ Java_com_example_mediadev_mooc_MoocActivity_mp4ToFlv(JNIEnv *env, jobject thiz,
         }
         av_packet_unref(&pkt);
     }
+    //写入尾部信息
     av_write_trailer(output_fmt_ctx);
+}
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_mediadev_mooc_MoocActivity_cutVideo(JNIEnv *env, jobject thiz, jstring input_path,
+                                                     jstring output_path, jdouble start_time,
+                                                     jdouble end_time) {
+    const char *input_file_path = env->GetStringUTFChars(input_path, 0);
+    const char *output_file_path = env->GetStringUTFChars(output_path, 0);
+
+    AVOutputFormat *ofmt = 0;
+    AVFormatContext *ifmt_ctx = 0;
+    AVFormatContext *ofmt_ctx = 0;
+    AVPacket pkt;
+    int ret, i;
+
+
+    if ((ret = avformat_open_input(&ifmt_ctx, input_file_path, NULL, NULL)) < 0) {
+        LOGD("打开文件失败");
+        return;
+    }
+    ret = avformat_find_stream_info(ifmt_ctx, NULL);
+    if (ret < 0) {
+        LOGD("获取输入文件的数据流失败");
+        return;
+    }
+    ret = avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, output_file_path);
+    if (ret < 0) {
+        LOGD("输出文件分配内存失败");
+        return;
+    }
+    ofmt = ofmt_ctx->oformat;
+
+    for (int i = 0; i < ifmt_ctx->nb_streams; ++i) {
+        AVStream *in_stream = ifmt_ctx->streams[i];
+        AVStream *out_stream = avformat_new_stream(ofmt_ctx, NULL);
+        if (!out_stream) {
+            LOGD("创建数据流失败");
+            return;
+        }
+        ret = avcodec_parameters_copy(out_stream->codecpar, in_stream->codecpar);
+        if (ret < 0) {
+            LOGD("数据流参数copy 失败");
+            return;
+        }
+        out_stream->codecpar->codec_tag = 0;
+    }
+    if (!(ofmt->flags & AVFMT_NOFILE)) {
+        ret = avio_open(&ofmt_ctx->pb, output_file_path, AVIO_FLAG_WRITE);
+        if (ret < 0) {
+            LOGD("创建IO流 上下文失败");
+            return;
+        }
+    }
+
+    ret = avformat_write_header(ofmt_ctx, NULL);
+    if (ret < 0) {
+        LOGD("写入头部信息失败");
+        return;
+    }
+
+    ret = av_seek_frame(ifmt_ctx, -1, start_time * AV_TIME_BASE, AVSEEK_FLAG_ANY);
+    if (ret < 0) {
+        LOGD("seek 失败");
+        return;
+    }
+    int64_t *dts_start_from = static_cast<int64_t *>(malloc(
+            sizeof(int64_t) * ifmt_ctx->nb_streams));
+
+    memset(dts_start_from, 0, sizeof(int64_t) * ifmt_ctx->nb_streams);
+
+    int64_t *pts_start_from = static_cast<int64_t *>(malloc(
+            sizeof(int64_t) * ifmt_ctx->nb_streams));
+
+    memset(pts_start_from, 0, sizeof(int64_t) * ifmt_ctx->nb_streams);
+
+    while (1) {
+        AVStream *in_stream;
+        AVStream *out_stream;
+
+        ret = av_read_frame(ifmt_ctx, &pkt);
+        if (ret < 0) {
+            break;
+        }
+        in_stream = ifmt_ctx->streams[pkt.stream_index];
+        out_stream = ofmt_ctx->streams[pkt.stream_index];
+
+        if (av_q2d(in_stream->time_base) * pkt.pts > end_time) {  //如果进度超过设置的截取时间则停止
+            av_packet_unref(&pkt);
+            break;
+        }
+        if (dts_start_from[pkt.stream_index] == 0) {
+            dts_start_from[pkt.stream_index] = pkt.dts;
+        }
+
+        if (pts_start_from[pkt.stream_index] == 0) {
+            pts_start_from[pkt.stream_index] = pkt.pts;
+        }
+        //时间基 转换
+        pkt.pts = av_rescale_q_rnd(pkt.pts - pts_start_from[pkt.stream_index],
+                                   in_stream->time_base, out_stream->time_base,
+                                   static_cast<AVRounding>(AV_ROUND_NEAR_INF |
+                                                           AV_ROUND_PASS_MINMAX));
+
+        pkt.dts = av_rescale_q_rnd(pkt.dts - dts_start_from[pkt.stream_index],
+                                   in_stream->time_base, out_stream->time_base,
+                                   static_cast<AVRounding>(AV_ROUND_NEAR_INF |
+                                                           AV_ROUND_PASS_MINMAX));
+
+        if (pkt.pts < 0) {
+            pkt.pts = 0;
+        }
+        if (pkt.dts < 0) {
+            pkt.dts = 0;
+        }
+        pkt.duration = (int)av_rescale_q((int64_t)pkt.duration, in_stream->time_base, out_stream->time_base);
+        pkt.pos = -1;
+
+        ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
+        if (ret < 0) {
+            LOGD("数据写入失败");
+            break;
+        }
+        av_packet_unref(&pkt);
+    }
+    free(dts_start_from);
+    free(pts_start_from);
+    av_write_trailer(ofmt_ctx);
+
 }
--- app/src/main/java/com/example/mediadev/mooc/MoocActivity.java
+++ app/src/main/java/com/example/mediadev/mooc/MoocActivity.java
@@ -2,7 +2,8 @@ package com.example.mediadev.mooc;
 
 import android.os.Bundle;
 import android.os.Environment;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,22 +15,44 @@ import com.example.mediadev.R;
  * Email: zhaoqirong96@gmail.com
  * Describe:
  */
-public class MoocActivity extends AppCompatActivity {
+public class MoocActivity extends AppCompatActivity implements View.OnClickListener {
     static {
         System.loadLibrary("native-lib");
     }
 
-    private TextView tvMeta;
+    private Button btnTransform;
+    private Button btnCutVideo;
+    private String inputPath;
+    private String outputPath;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mooc);
-        tvMeta = findViewById(R.id.tv_meta);
-        String inputPath = Environment.getExternalStorageDirectory() + "/input.mp4";
-        String outPath = Environment.getExternalStorageDirectory() + "/output.flv";
-        mp4ToFlv(inputPath, outPath);
+        btnTransform = findViewById(R.id.btn_transform);
+        btnCutVideo = findViewById(R.id.btn_cut_video);
+        btnTransform.setOnClickListener(this);
+        btnCutVideo.setOnClickListener(this);
+        inputPath = Environment.getExternalStorageDirectory() + "/input.mp4";
+        outputPath = Environment.getExternalStorageDirectory() + "/output.mp4";
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_transform:
+                mp4ToFlv(inputPath, outputPath);
+                break;
+            case R.id.btn_cut_video:
+                double startTime = 5;
+                double endTime = 20;
+                cutVideo(inputPath, outputPath, startTime, endTime);
+                break;
+        }
     }
 
     public native void mp4ToFlv(String path, String outputPath);
+
+    private native void cutVideo(String inputPath, String outputPath, double startTime, double endTime);
+
 }
